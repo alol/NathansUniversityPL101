1ae06a2798be527ad8958f7c4b41750add470dfa
Alan Oliver <alanoliver@gmail.com>
2012-07-31 00:00:46 +0100

    1) add rests 2) change to use switch instead of 'if's

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,37 +1,48 @@
 var endTime = function (time, expr) {
-    if (expr.tag == 'note') {
-        return time + expr.dur;
-    } else if (expr.tag == 'par') {
-        eLeft = endTime(time, expr.left);
-        eRight = endTime(time, expr.right);
-        return eLeft >= eRight ? eLeft : eRight;
-    } else {
-        return endTime(time, expr.left) + 
-            endTime(time, expr.right);
+    switch(expr.tag) { 
+        case 'note':
+        case 'rest':
+            return time + expr.dur;
+        case 'par':
+            eLeft = endTime(time, expr.left);
+            eRight = endTime(time, expr.right);
+            return eLeft >= eRight ? eLeft : eRight;
+            break;
+        case 'seq':
+            return endTime(time, expr.left) + 
+                endTime(time, expr.right);
+            break;
     }
 };
 
 var noteForMusStatement = function(expr, time, note) {
-    if (expr.tag == 'note') {
-        note.push({ tag: 'note',
-                pitch: expr.pitch,
-                start: time,
-                dur: expr.dur
-        });
-    } else if (expr.tag == 'par'){
-        note = noteForMusStatement(expr.left, 
-                            time, 
-                            note);
-        note = noteForMusStatement(expr.right, 
-                            time, 
-                            note);
-    } else {
-        note = noteForMusStatement(expr.left, 
-                            time, 
-                            note);
-        note = noteForMusStatement(expr.right, 
-                            endTime(time, expr.left), 
-                            note);
+    switch(expr.tag) {
+        case 'note':
+            note.push({ tag: 'note',
+                    pitch: expr.pitch,
+                    start: time,
+                    dur: expr.dur
+            });
+            break;
+        case 'rest':
+            note.push({ tag: 'rest', start: time, dur: expr.dur});
+            break;
+        case 'par':
+            note = noteForMusStatement(expr.left, 
+                                time, 
+                                note);
+            note = noteForMusStatement(expr.right, 
+                                time, 
+                                note);
+            break;
+        case 'seq':
+            note = noteForMusStatement(expr.left,
+                                time, 
+                                note);
+            note = noteForMusStatement(expr.right, 
+                                endTime(time, expr.left), 
+                                note);
+            break;
     }
     return note;
 };
@@ -43,13 +54,13 @@ var compile = function (musexpr) {
 var melody_mus = 
     { tag: 'seq',
       left: 
-       { tag: 'seq',
+       { tag: 'par',
          left: { tag: 'note', pitch: 'a4', dur: 250 },
          right: { tag: 'note', pitch: 'b4', dur: 250 } },
       right:
        { tag: 'seq',
          left: { tag: 'note', pitch: 'c4', dur: 500 },
-         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+         right: { tag: 'rest', dur: 500 } } };
 
 console.log(melody_mus);
 console.log(compile(melody_mus));
