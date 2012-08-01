20882d60bef842f50bd8f45cd5092d76fa233d2f
Alan Oliver <alanoliver@gmail.com>
2012-08-01 17:21:22 +0100

    Add repeats

--- peg/muso/test_muso.js
+++ peg/muso/test_muso.js
@@ -25,3 +25,22 @@ assert.deepEqual( parse("g3 for 500ms and\nc3 for 250ms"),
         } );
 //rest
 assert.deepEqual( parse("rest for 500ms"), {tag:"rest", dur:500} );
+
+assert.deepEqual( parse("2 repeats do\nc3 for 250ms\ne3 for 250ms\nend"), 
+    {
+      "tag": "repeat",
+      "count": "2",
+      "section": {
+               "tag": "seq",
+         "left": {
+                     "tag": "note",
+            "pitch": "c3",
+            "dur": "250"
+         },
+         "right": {
+                     "tag": "note",
+            "pitch": "e3",
+            "dur": "250"
+         }
+   }
+});
